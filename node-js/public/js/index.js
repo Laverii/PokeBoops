a655b896ebe6e4a5cd34b63fe32aad37ad126f3c
Laverii <phoenixpyre_xx@yahoo.com>
2017-05-24 01:33:22 -0700

    Inkan: Modified draw circle function. When a circle is drawn, user can press the anywhere in the circle to open a popup which asks the user if the item is lost or found. Pressing the found button will let the user fill out the found form and it will post on the map. Similary, this process works with lost.

--- node-js/public/js/index.js
+++ node-js/public/js/index.js
@@ -54,47 +54,135 @@ function load_map() {
 				layer = e.layer;
 				if(type == 'circle')
 				{
-					 //PopupL http://leafletjs.com/reference-1.0.3.html#popup
-                     //Inkan: lostFound still a work in progress. Maybe a popup will first appear to ask if the user is inputting infor a lost or found object, and once they press it will take them to the appropriate form?   
-                        /*var lostFound = new L.popup();
-                        lostFound.setContent('<p>Is your item: <input type ="button" name = "goToLost" value = "Lost" id = "goto_lostForm"></input> or <input type= "button" name = "goToFound" value = "Found" id = "goto_foundForm"></input>');
-                        layer.bindPopup(lostFound);*/
+					//Popup: http://leafletjs.com/reference-1.0.3.html#popup
+                    //Inkan: THis popup asks if the item is lost or found.
+                    var lostFound = new L.popup();
+                    lostFound.setContent('<p>Is your item: <input type ="button" name = "goToLost" value = "Lost" id = "goto_lostForm"></input> or <input type= "button" name = "goToFound" value = "Found" id = "goto_foundForm"></input>');
+                    layer.bindPopup(lostFound);
 
-                        var popup = new L.popup();
-                        //switch input type for the submit button, went from "submit" to button.
-                        popup.setContent('<form id ="found_Form"> <p>Item Name:</p> <input type ="text" name="item_name" id = "item_name"></text><br><p>Description</p><br><textarea rows="6" cols="20" name = "description" id = "description"></textarea><p>If you lost this item, please contact:</p><br>Full Name: <input type ="text" name = "full_name" id ="full_name"></input><br><p>Phone number:</p> <input type = "text" name = "phone_number" id = "phone_number"></input><br><p>Email:</p><input type = "text" name = "email" id = "email"></input><br><input type = "button" name = "Post Found Item" value ="submit" id ="submit_Id"></input></form>');
-                        //Inkan: THis part sets up the the popup to show a list of all the information that was submitted on the found form. Commented it out because not really working.
-                        /*popup.setContent('<ul class="list-group"><% for(var i=0; i<data.length; i++) {%><li class = "list-group-item">Item Name: <%= data[i].item_name %><br><%= data[i].description %></br><br>Name: <%= data[i].full_name %></br><br>Phone Number: <%= data[i].phone_number %></br><br>Email: <%= data[i].email %></br></li><% } %></ul>');*/
-                        layer.bindPopup(popup);
-                        var popupOpenFunction = function() {
-                        var button = document.getElementById('submit_Id');
+                    //found button clicked, it will open the form for found.
+                    var foundClicked = function() {
+                        //Get the submit button for found
+                        var foundButton = document.getElementById('goto_foundForm');
+                            //When clicked change the popup to the found form.
+                            foundButton.onclick = function() {
+                                var popup = new L.popup();
+                                popup.setContent('<form id ="found_Form"> <p>Item Name:</p> <input type ="text" name="item_name" id = "item_name"></text><br><p>Description</p><br><textarea rows="6" cols="20" name = "description" id = "description"></textarea><p>If you LOST this item, please contact:</p><br>Full Name: <input type ="text" name = "full_name" id ="full_name"></input><br><p>Phone number:</p> <input type = "text" name = "phone_number" id = "phone_number"></input><br><p>Email:</p><input type = "text" name = "email" id = "email"></input><br><input type = "button" name = "Post Found Item" value ="submit" id ="submit_Id"></input></form>');
+                                //layer.bindPopup(popup);
+                                    
+                                //Occurs when you submit the form. It takes that information and puts it on the popup.
+                                var submittedFoundForm = function() {   
+                                //Gets submit button and gets popup elements
+                                var button = document.getElementById('submit_Id');
+                                button.onclick = function () {
+                                    //GetElementById form values: http://stackoverflow.com/questions/3547035/javascript-getting-html-form-values
+                                    //Get form elements
+                                    var itemName = document.getElementById('item_name').value;
+                                    var description = document.getElementById('description').value;
+                                    var fullName = document.getElementById('full_name').value;
+                                    var phoneNumber = document.getElementById('phone_number').value;
+                                    var email_address = document.getElementById('email').value;
+                                    //Make a new popup to print it all up
+                                    var finalPopup = L.popup();
+                                    //Set the popup with the new infromation
+                                    finalPopup.setContent('<p>Item Name: ' + itemName + '<br>Description: ' +description+ '<br>If you LOST this please contact: <br> Name:' +fullName + '<br>Phone Number: '+ phoneNumber + '<br>Email: ' +email_address+ '</p>');
+                                
 
-                        button.onclick = function () {
-                                 //GetElementById form values: http://stackoverflow.com/questions/3547035/javascript-getting-html-form-values
+                                    //Closing popups: https://github.com/Leaflet/Leaflet/issues/1612
+                                    //._popUp: A function of layer, allows it to grab the popup binded to it
+                                    lostMap.closePopup(layer._popup);
+
+                                    //closes old popup of the form and opens the new one with the information.
+                                    layer.unbindPopup();
+                                    layer.bindPopup(finalPopup);
+                                    //Adds popup http://leafletjs.com/reference-1.0.3.html#layer
+                                    lostMap.openPopup(finalPopup, layer.getLatLng());
+                           
+                                };
+                                //Takes off previous listener which is the form: http://leafletjs.com/reference-1.0.3.html#map-event
+                                lostMap.off('popupopen', submittedFoundForm);
+                            };
+                            //Adds on new listener, which is the completed form: http://leafletjs.com/reference-1.0.3.html#map-event
+                            lostMap.on('popupopen', submittedFoundForm);
+
+                            //Closes popup with the question that asks if the item is lost or found.
+                            lostMap.closePopup(layer._popup);
+
+                            //removes the old popup and puts in new one
+                            layer.unbindPopup();
+                            layer.bindPopup(popup);
+
+                            //opens the opup on the map.
+                            lostMap.openPopup(popup, layer.getLatLng());
+                        };
+                        //Takes off the listener for the button asking if form is lost or found.
+                        lostMap.off('popupopen', foundClicked);
+                        };
+                        //Adds listener which is that the found button was clicked.
+                        lostMap.on('popupopen', foundClicked);
+
+                    //found button clicked, it will open the form for found.
+                    var lostClicked = function() {
+                    //Get the submit button for found
+                        var foundButton = document.getElementById('goto_lostForm');
+                         //When clicked change the popup to the found form.
+                        foundButton.onclick = function() {
+                            var popup = new L.popup();
+                            popup.setContent('<form id ="found_Form"> <p>Item Name:</p> <input type ="text" name="item_name" id = "item_name"></text><br><p>Description</p><br><textarea rows="6" cols="20" name = "description" id = "description"></textarea><p>If you FOUND this item, please contact:</p><br>Full Name: <input type ="text" name = "full_name" id ="full_name"></input><br><p>Phone number:</p> <input type = "text" name = "phone_number" id = "phone_number"></input><br><p>Email:</p><input type = "text" name = "email" id = "email"></input><br><input type = "button" name = "Post Found Item" value ="submit" id ="submit_Id"></input></form>');
+                            //layer.bindPopup(popup);
+                                    
+                            //Occurs when you submit the form. It takes that information and puts it on the popup.
+                            var submittedlostForm = function() {   
+                                //Gets submit button and gets popup elements
+                                var button = document.getElementById('submit_Id');
+                                button.onclick = function () {
+                                //GetElementById form values: http://stackoverflow.com/questions/3547035/javascript-getting-html-form-values
+                                //Get form elements
                                 var itemName = document.getElementById('item_name').value;
                                 var description = document.getElementById('description').value;
                                 var fullName = document.getElementById('full_name').value;
                                 var phoneNumber = document.getElementById('phone_number').value;
                                 var email_address = document.getElementById('email').value;
+                                //Make a new popup to print it all up
                                 var finalPopup = L.popup();
-                                finalPopup.setContent('<p>Item Name: ' + itemName + '<br>Description: ' +description+ '<br>If you lost this please contact: <br> Name:' +fullName + '<br>Phone Number: '+ phoneNumber + '<br>Email: ' +email_address+ '</p>');
+                                //Set the popup with the new infromation
+                                finalPopup.setContent('<p>Item Name: ' + itemName + '<br>Description: ' +description+ '<br>If you FOUND this please contact: <br> Name:' +fullName + '<br>Phone Number: '+ phoneNumber + '<br>Email: ' +email_address+ '</p>');
+                                
 
                                 //Closing popups: https://github.com/Leaflet/Leaflet/issues/1612
                                 //._popUp: A function of layer, allows it to grab the popup binded to it
                                 lostMap.closePopup(layer._popup);
 
-                                //closes old popup and opens the submitted forms.
+                                //closes old popup of the form and opens the new one with the information.
                                 layer.unbindPopup();
                                 layer.bindPopup(finalPopup);
                                 //Adds popup http://leafletjs.com/reference-1.0.3.html#layer
                                 lostMap.openPopup(finalPopup, layer.getLatLng());
+                           
+                                };
+                                //Takes off previous listener which is the form: http://leafletjs.com/reference-1.0.3.html#map-event
+                                lostMap.off('popupopen', submittedlostForm);
                             };
-                            //Takes off previous listener which is the form: http://leafletjs.com/reference-1.0.3.html#map-event
-                            lostMap.off('popupopen', popupOpenFunction);
+                            //Adds on new listener, which is the completed form: http://leafletjs.com/reference-1.0.3.html#map-event
+                            lostMap.on('popupopen', submittedlostForm);
+
+                            //Closes popup with the question that asks if the item is lost or found.
+                            lostMap.closePopup(layer._popup);
+
+                            //removes the old popup and puts in new one
+                            layer.unbindPopup();
+                            layer.bindPopup(popup);
+
+                            //opens the opup on the map.
+                            lostMap.openPopup(popup, layer.getLatLng());
+                            };
+                            //Takes off the listener for the button asking if form is lost or found.
+                            lostMap.off('popupopen', lostClicked);
                         };
-                        //Adds on new listener, which is the completed form: http://leafletjs.com/reference-1.0.3.html#map-event
-                       lostMap.on('popupopen', popupOpenFunction);
-				}
+                        //Adds listener which is that the found button was clicked.
+                        lostMap.on('popupopen', lostClicked);
+                            
+				    }//end of circle for loop
 				if(type == 'polyline')
 				{
 					//add marker/popup here?
