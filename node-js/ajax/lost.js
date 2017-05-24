550e93a6c5f3adc6fb1c97d9ea2f848794b85854
Peter Tran <ptsd1994@hotmail.com>
2017-05-24 11:13:24 -0700

    phtran: I modded lost.js to be like found.js.

--- node-js/ajax/lost.js
+++ node-js/ajax/lost.js
@@ -1,8 +1,14 @@
+//Kandarp and Alok helped us here
+var fs = require('fs');
+var d = require("./data.json");
+
 module.exports = {
 	lostItem: function(req, res){
+		
 		console.log("enter lost js");
-		require("./data.json").push(req.body);
-		console.log(require("./data.json"));
+		d.push(req.body);
+		fs.writeFile('ajax/data.json', JSON.stringify(d));
+		console.log(d)
 		res.success();
 	}
 }
